b49f800a79c6f4cf0f03f406ae5748e921bca4cb
Emilio Madrid <emadridabarca@gmail.com>
2024-03-28 20:46:13 -0300

    Documentación

--- script.py
+++ script.py
@@ -1,14 +1,29 @@
+# Librerias
 import json
 
 class Usuario:
+    """ Clase que representa el objeto Usuario con su información básica.
+    """
     def __init__(self, nombre: str, apellido: str, email: str, genero: str) -> None:
+        """ Metodo que inicializa una nueva instancia de Usuario.
+
+        Args:
+            nombre (str): Cadena de texto correspondiente al nombre del usuario.
+            apellido (str): Cadena de texto correspondiente al apellido del usuario.
+            email (str): Cadena de texto correspondiente al correo del usuario.
+            genero (str): Cadena de texto correspondiente al genero del usuario.
+        """
         self.nombre = nombre
         self.apellidos = apellido
         self.email = email
         self.genero = genero
 
 def main():
+    """
+    Funcion que ejecuta el código.
+    """
     usuarios = []
+    # Lectura del archivo txt y almacenamiento del texto.
     with open('usuarios.txt', 'r') as archivo_usuarios:
         for linea in archivo_usuarios:
             try:
@@ -19,9 +34,10 @@ def main():
                 with open('error.log', 'a') as archivo_log:
                     archivo_log.write(f'Error al procesar la línea: {linea}\nExcepción: {str(e)}\n')
 
-    # Opcional: imprimir las instancias de Usuario para verificar
+    # Impresión de las instancias para verificación.
     for usuario in usuarios:
         print(f'Nombre: {usuario.nombre}, Apellido: {usuario.apellidos}, Email: {usuario.email}, Género: {usuario.genero}')
 
+# Test unitario.
 if __name__ == '__main__':
     main()
